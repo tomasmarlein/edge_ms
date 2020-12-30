57ec831872603db5cde765ba6dd002f5deab53de
vanhouttejan4 <38554039+vanhouttejan4@users.noreply.github.com>
2020-12-30 13:58:52 +0100

    fix setup

---
+++ src/main/java/fact/it/edgems/EdgemsApplication.java
@@ -0,0 +1,13 @@
+package fact.it.edgems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/edgems/EdgemsApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.edgems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
