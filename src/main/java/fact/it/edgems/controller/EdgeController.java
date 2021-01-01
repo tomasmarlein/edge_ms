13cf9d9acd0659b5f4071c3f4bb85fdcbe207bbe
Tom Bosmans <r0621531@student.thomasmore.be>
2021-01-01 12:09:19 +0100

    moved watchlist endpoints to the EdgeController

--- src/main/java/fact/it/edgems/controller/EdgeController.java
+++ src/main/java/fact/it/edgems/controller/EdgeController.java
@@ -1,6 +1,7 @@
 package fact.it.edgems.controller;
 
 import fact.it.edgems.model.Review;
+import fact.it.edgems.model.Watchlist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -77,5 +78,61 @@ public class EdgeController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/watchlist/all")
+    public List<Watchlist> getAll(){
+        ResponseEntity<List<Watchlist>> responseEntityWatchlist =
+                restTemplate.exchange("http://" + watchlistmsBaseUrl + "/watchlist/all",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Watchlist>>() {
+                        });
+
+        return responseEntityWatchlist.getBody();
+    }
+
+    @GetMapping("/watchlist/{uuid}")
+    public Watchlist getOne(@PathVariable UUID uuid){
+        ResponseEntity<Watchlist> responseEntityWatchlist =
+                restTemplate.exchange("http://" + watchlistmsBaseUrl + "/watchlist/" +uuid.toString(),
+                        HttpMethod.GET, null, new ParameterizedTypeReference<Watchlist>() {
+                        });
+
+        return responseEntityWatchlist.getBody();
+    }
+
+    @GetMapping("/watchlist/watched")
+    public List<Watchlist> getWatchlistWatched(){
+        ResponseEntity<List<Watchlist>> responseEntityWatchlist =
+                restTemplate.exchange("http://" + watchlistmsBaseUrl + "/watchlist/watched",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Watchlist>>() {
+                        });
+
+        return responseEntityWatchlist.getBody();
+    }
+
+    @PostMapping("watchlist/{movieUuid}")
+    public Watchlist add(@PathVariable UUID movieUuid){
+        ResponseEntity<Watchlist> responseEntityWatchlist =
+                restTemplate.exchange("http://" + watchlistmsBaseUrl + "/watchlist/"+movieUuid,
+                        HttpMethod.POST, null, new ParameterizedTypeReference<Watchlist>() {
+                        });
+
+        return responseEntityWatchlist.getBody();
+    }
+
+    @PutMapping("watchlist/watched/{uuid}")
+    public Watchlist setWatched(@PathVariable UUID uuid){
+        ResponseEntity<Watchlist> responseEntityWatchlist =
+                restTemplate.exchange("http://" + watchlistmsBaseUrl + "/watchlist/"+uuid,
+                        HttpMethod.PUT, null, new ParameterizedTypeReference<Watchlist>() {
+                        });
+
+        return responseEntityWatchlist.getBody();
+    }
+
+    @DeleteMapping("watchlist/{movieUuid}")
+    public ResponseEntity delete(@PathVariable UUID movieUuid){
+        restTemplate.delete("http://" + watchlistmsBaseUrl + "/watchlist/" + movieUuid);
+
+        return ResponseEntity.ok().build();
+    }
 
 }
